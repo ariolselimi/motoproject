3f731a72b8a3a9a74b9e53e5d553aa4a5f25a73a
arioli <you@example.com>
2022-12-24 11:54:50 +0100

    motoproject

---
+++ java.js
@@ -0,0 +1,18 @@
+
+        let redbtn = document.getElementById("red");
+        let bluebtn = document.getElementById("blue");
+        let blackbtn = document.getElementById("black");
+        let bike = document.getElementById("bike");
+
+        
+        redbtn.onclick=function(){
+            bike.style.backgroundImage ="url(img/BMW1.png)";
+
+        }
+        bluebtn.onclick=function(){
+            bike.style.backgroundImage = "url(img/BMW2.png)"
+        }
+        blackbtn.onclick=function(){
+            bike.style.backgroundImage = "url(img/BMW3.png)";
+        }
+    
